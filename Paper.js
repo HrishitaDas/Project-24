830f858bd083443533fbd6a4df5a68d9366e2dcb
HrishitaDas <74808543+HrishitaDas@users.noreply.github.com>
2020-11-21 11:29:01 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,22 @@
+class Paper{
+    constructor(){
+        var option = {
+            isStatic:false,
+            restitution: 0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body=Bodies.circle(100,650,30,option);
+        this.radius=30;
+        World.add(world, this.body);
+    }
+        display(){
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            fill("pink");
+            ellipseMode(CENTER);
+            ellipse(pos.x,pos.y,this.radius,this.radius);
+
+        }
+
+}
---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var paper;
+var ground;
+var dustbinbottom,dustbinleft,dustbinright;
+
+
+function setup() {
+  createCanvas(800,700);
+  engine = Engine.create();
+  world = engine.world;
+
+  paper = new Paper();
+  ground = new Ground(400,650,800,10);
+  dustbinbottom = new Dustbin(600,635,200,20);
+  dustbinleft = new Dustbin(510,575,20,100);
+  dustbinright = new Dustbin(690,575,20,100);
+
+
+  
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  
+  paper.display();
+  ground.display();
+  dustbinright.display();
+  dustbinleft.display();
+  dustbinbottom.display();
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW ){
+    Matter.Body.applyForce(paper.body, paper.body.position,{x:180,y:-110});
+  }
+}
